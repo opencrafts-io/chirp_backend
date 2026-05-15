9c5488885bf83c16269b1d7fbb0646ef42106d1d
Erick <hearteric57@gmail.com>
2026-05-15 17:10:27 +0300

    fix(eventbus): removes threading support for celery event bus

--- chirp/settings.py
+++ chirp/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-from event_bus import publisher
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -349,5 +347,3 @@ LOGGING = {
         },
     },
 }
-
-publisher.register_shutdown_hook()
