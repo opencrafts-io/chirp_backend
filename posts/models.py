d81babf067a335194f2f208b3c6bced0876cd6fa
NgigiN <ngigi.nyongo@gmail.com>
2026-05-23 01:01:26 +0300

    perf: optimize feed response times by adding database indexes to Post model

--- posts/models.py
+++ posts/models.py
@@ -102,6 +102,11 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["created_at"], name="post_created_at_idx"),
+            models.Index(fields=["community"], name="post_community_idx"),
+            models.Index(fields=["author"], name="post_author_idx"),
+        ]
 
 
 class PostView(models.Model):
